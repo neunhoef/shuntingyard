a6b3e79ae3c0035caa367a194c8fb3f3c1c1e1a4
Max Neunhoeffer <max@arangodb.com>
2017-04-13 09:15:24 +0200

    Some thoughts on eval added.

--- Eval.h
+++ Eval.h
@@ -4,13 +4,15 @@
 
 // Here is the complete set of evaluation rules:
 //
-// (1) Leafs evaluate to themselves, unless they are identifiers.
-// (2) Identifiers are looked up in the name space of the Eval object
+// (1) Representations of builtin types evaluate to the underlying value.
+// (2) Every evaluation happens in a context, which defines two local name
+//     spaces, one for identifiers and for functions and operators.
+// (3) Identifiers are first looked up in the name space of the Eval object
 //     and then in the parent name space and so on until there is no
 //     more parent, and then the _system name space is taken. If found
 //     the result is the expression found, otherwise the result is nil.
-// (3) Operator and function expressions perform a lookup in the function
-//     name space.
+// (4) Operator and function expressions perform a lookup in the function
+//     name space, then go to parents, and finally to the system name space.
 //
 // Here are some special functions that are useful:
 
